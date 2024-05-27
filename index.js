b1ec43f99e055019c749abd06f6ce708b39bdbdd
mochamochaccino <156242839+mochamochaccino@users.noreply.github.com>
2024-05-27 00:24:48 +0000

    Completion of Project

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 class player {
-  constructor(name) {
-    this.name = name;
-    this.maxHealth = 100;
+  constructor(name, health) {
+    (this.name = name), (this.health = health), (this.maxHealth = 100);
   }
   takeDamage(damageTaken) {
     this.health -= damageTaken;
-    return "Your health is " + this.health;
+    return `${this.name} has taken ${damageTaken} damage`;
   }
   healDamage() {
     this.health = this.maxHealth;
@@ -13,17 +12,19 @@ class player {
   }
 }
 class Warrior extends player {
-  constructor(name) {
-    super(name);
+  constructor(name, health) {
+    super(name, health);
     this.strength = 15;
     this.intelligence = 5;
+    this.output = Math.floor(this.strength * 1.5 + this.intelligence * 1);
   }
 }
 class Mage extends player {
-  constructor(name) {
-    super(name);
+  constructor(name, health) {
+    super(name, health);
     this.strength = 5;
     this.intelligence = 15;
+    this.output = Math.floor(this.strength * 1 + this.intelligence * 1.5);
   }
 }
 class Monster extends player {
@@ -31,63 +32,117 @@ class Monster extends player {
     super(name, health);
     this.attackPower = attackPower;
   }
-  attack() {
-    takeDamage(this.attackPower); // not working
-  }
 }
 
 function userInput(user) {
   const prompt = require("prompt-sync")({ sigint: true });
   return prompt(user);
+  //APPARENTLY THERES AN ISSUE WITH PROMPT-SYNC WHERE LONG STRINGS BREAK THINGS. THANK YOU STACKOVERFLOW
+}
+
+function inputTest(input1, input2, response, invalid) {
+  if (response.toUpperCase() !== input1 && response.toUpperCase() !== input2) {
+    console.log(invalid);
+  } else if (response.toUpperCase() === input1) {
+    return input1;
+  } else if (response.toUpperCase() === input2) {
+    return input2;
+  }
 }
 
 function introduction() {
   while (true) {
     let intro =
-      "Welcome adventurer. Please select your class: Warrior or Mage:  ";
-    let response = userInput(intro).trim();
-    if (response !== "Warrior" && response !== "Mage") {
-      console.log("Invalid response. Please select between Warrior or Mage: ");
-    } else if (response === "Warrior") {
+      "Welcome adventurer. Please select your class: Warrior or Mage: ";
+    let response = userInput(intro);
+    //console.log(response.toUpperCase());
+    let invalid = "Invalid response. Please select between Warrior or Mage \n";
+    const decision = inputTest("WARRIOR", "MAGE", response, invalid);
+    //console.log(decision);
+    if (decision === "WARRIOR") {
+      war = new Warrior("Warrior", 80);
       console.log("You have selected: " + response);
-      war = new Warrior("Warrior");
       return war;
-    } else if (response === "Mage") {
+    }
+    if (decision === "MAGE") {
+      mage = new Mage("Mage", 80);
       console.log("You have selected: " + response);
-      mage = new Mage("Mage");
       return mage;
     }
   }
 }
-let person = introduction();
-console.log(person);
+
+//APPARENTLY THERES AN ISSUE WITH PROMPT-SYNC WHERE LONG STRINGS BREAK THINGS. THANK YOU STACKOVERFLOW. CONSOLE.LOG LONG STRINGS.
 
 function town() {
-  while (true) {
-    let intro =
-      "Welcome to Dweller Town. You must defeat the slime that is preventing people from leaving the town. You are in town square. What would you like to do? To the Inn or Battle? ";
-    let response = userInput(intro);
-    if (response !== "Inn" && "Battle") {
-      console.log("Invalid response. Please select Inn or Battle");
-    } else if (response === "Inn") {
-      console.log(person.healDamage());
+  const person = introduction();
+  const combat = battle();
+  let second = false;
+  //console.log(person);
+  function closure() {
+    while (true) {
+      if (!second) {
+        console.log(
+          "\nWelcome to Dweller Town. You must defeat the slime that is preventing people from leaving the town. You are in town square. What would you like to do? To the Inn or Battle? \n",
+        );
+      } else {
+        console.log(`\nWelcome back to town. Your HP is ${person.health}`);
+      }
+      second = true;
+      let invalid = "Invalid response. Please select Inn or Battle";
+      let response = userInput().toUpperCase();
+      const decision = inputTest("INN", "BATTLE", response, invalid);
+      if (decision === "INN") {
+        console.log(person.healDamage());
+      }
+      if (decision === "BATTLE") {
+        if (combat(person) === true) {
+          break;
+        }
+      }
     }
+    console.log("\nVictory!");
   }
+  return closure;
 }
-
-town();
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
 function battle() {
-  //mob = new Monster("slime", 20, 5);
-  let engagedInBattle = "";
-  while (engagedInBattle !== "fight" || engagedInBattle !== "flee") {
-    engagedInBattle = prompt("You have encountered a monster. Fight or flee?!");
+  mob = new Monster("Super Slime", 100, 30);
+  function closure(personObj) {
+    while (true) {
+      let intro = `A fierce slime is blocking your path. You have ${personObj.health}. It has ${mob.health} HP. What do you do? Fight/Flee `;
+      let response = userInput(intro).toUpperCase();
+      let invalid = "Invalid response. Please select Fight or Flee";
+      const decision = inputTest("FIGHT", "FLEE", response, invalid);
+      if (decision === "FIGHT") {
+        console.log(
+          mob.takeDamage(personObj.output + Math.floor(randomNumber(-5, 5))),
+        );
+        console.log(
+          personObj.takeDamage(
+            mob.attackPower + Math.floor(randomNumber(-5, 5)),
+          ),
+        );
+        if (personObj.health < 0) {
+          console.log("You were knocked out. You find yourself at the town");
+          personObj.health = 1;
+          return 0;
+        }
+        if (mob.health < 0) {
+          return true;
+        }
+      }
+      if (decision === "FLEE") {
+        return 0;
+      }
+    }
   }
-  if (engagedInBattle === "fight") {
-  } else {
-  }
+  return closure;
 }
+
+const adventure = town();
+adventure();
